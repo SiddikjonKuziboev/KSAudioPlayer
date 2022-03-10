8c72b608bbef1b57cff4ea045834e6b912a36930
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2022-03-10 17:36:50 +0500

    Initial Commit

---
+++ KSAudioPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KSAudioPlayer
+//
+//  Created by Kuziboev Siddikjon on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
